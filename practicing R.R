2f93640f07e6c63dd5b75f1257507e8380afc6ce
Babar Nadeem <babarnadeem996@gmail.com>
2021-09-28 22:18:59 -0700

    Intro

---
+++ practicing R.R
@@ -0,0 +1,23 @@
+# Data Types
+a <- 5
+print(a)
+# What is the class of object?
+class(a)
+
+a <- 5L
+class(a)
+
+a <- 5.332
+class(a)
+
+a <- 5.32L
+class(a)
+
+a <- 5K  #Not Possible with K. 
+class(a)
+
+b <- 1/0
+print(b)
+
+c <- 0/0
+print(c)
