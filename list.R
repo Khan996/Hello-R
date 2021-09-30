ca9c07cb7c2c18376671acd02d574c741cb8beae
Babar Nadeem <babarnadeem996@gmail.com>
2021-09-29 23:03:58 -0700

    list practice; only intro

---
+++ list.R
@@ -0,0 +1,11 @@
+# Lists
+
+# Forexample
+a <- c(1,2,3,4, "A", "B")
+a
+# All are converted to characters
+# To maintain the data type smae we use list in R
+
+A <- list(1,2,3,4,5, "A", "Babar", TRUE, FALSE)
+A
+# Now data type is not changed. That's why list are used. 
