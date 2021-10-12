f8a245d58fc4acab46f60a31efbc90bf5b499398
Babar Nadeem <babarnadeem996@gmail.com>
2021-10-12 08:26:57 -0700

    matrix dimnames

--- Data Frames.R
+++ Data Frames.R
@@ -16,3 +16,25 @@ y <- data.frame(Students = 1:5,
 
 y
 
+z <- 1:3
+names(z) <- c("Col 1", "Col 2", "Col 3")
+z
+names(z)
+
+k <- list(a=1, b=2, c=3)
+k
+$a # gives error
+
+l <- list(a=1:5, b= "ID", c = c("First Name", "Last Name" ))
+l
+names(z)
+names(x)
+names(l)
+l$c
+x$Was
+y$Location
+
+m <- matrix(1:4, nrow = 2, ncol =2)
+m
+dimnames(m) <- list(c("a", "b"), c("c", "d"))
+m
