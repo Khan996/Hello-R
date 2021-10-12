cc91be1e346aa1eeb5941da929f2bd11fd29bf31
Babar Nadeem <babarnadeem996@gmail.com>
2021-10-12 04:43:22 -0700

    Data Frames

---
+++ Data Frames.R
@@ -0,0 +1,18 @@
+# Data Frames
+
+x <- data.frame(Day = 1:5, 
+                Rain = c("Babar","Awaid","Waseem","Zulfi","Ali"),
+                Was = c("Babar","Awaid","Waseem","Zulfi","Ali"))
+x
+nrow(x
+     )
+ncol(x)
+
+y <- data.frame(Students = 1:5,
+                Name = c("Babar", "Awaid", "Aftab","Usman", "Ali"),
+                Status = c("Data Scientist", "Servant", "Unique", "Servant", 
+                           "Chemical Engr"),
+               Location = c("Berlin", "Lahore", "Peshawar", "Multan", "KPk"))
+
+y
+
